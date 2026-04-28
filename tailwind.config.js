9bae0e6d8d18ad3f7357d54d9c535b74102a6773
wajahatk <65616612+wajahatk@users.noreply.github.com>
2026-04-28 22:31:56 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
+  plugins: {
+    '@tailwindcss/postcss': {},
+    'autoprefixer': {},
   },
-  plugins: [],
 }
